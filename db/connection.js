6b1970a7c90b111fed3e4dfa136f95692d8a1d77
Artem Milevskyi <milevskyi.a@gmail.com>
2024-03-02 07:05:46 -0800

    Fix connect db

--- db/connection.js
+++ db/connection.js
@@ -4,8 +4,7 @@ const env = require("dotenv").config();
 const connectDb = async () => {
   try {
     const env = process.env.ENV;
-    const mongo_url =
-      env === "dev" ? process.env.MONGODB_URL : process.env.AZURE_MONGODB_URL;
+    const mongo_url = process.env.MONGODB_URL;
     const connect = await mongoose.connect(mongo_url);
     console.log("Database connected: ");
   } catch (err) {
